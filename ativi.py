fa795a7c200c1fcf39ba05d9c9a39a1aa226b7f0
Yehudi Dev <yehudidev@gmail.com>
2024-11-13 20:00:35 -0300

    ativi.py

---
+++ ativi.py
@@ -0,0 +1,20 @@
+
+print("olá, qual é o seu nome?")
+
+
+nome = input(":")
+
+print("qual é foi o mês de compra?")
+
+mes = (input(":"))
+
+print("qual foi o valor da sua compra?")
+
+valor_de_compra =float(input(":"))
+
+cupom_de_desconto = "É10"
+
+desconto = 10
+
+
+print(f"olá, {nome} em {mes} você realizou uma compra no valor de {valor_de_compra} e ganhou um desconto de {desconto}%, em sua proxima compra. use o cupom {nome + cupom_de_desconto} ")
